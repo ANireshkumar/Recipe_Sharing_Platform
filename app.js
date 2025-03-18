2f103ee494463eb3318c5ed2fbc5235f90481a55
ANireshkumar <niresh1606@gmail.com>
2025-03-18 06:23:07 +0530

    posts --crud endpoints done

--- app.js
+++ app.js
@@ -4,21 +4,27 @@ const cookieParser = require('cookie-parser');
 const errorRoute = require('./middlewares/errorRoute');
 const authRouter = require('./routes/authRoutes');
 const cors = require('cors');
+const postRouter = require('./routes/postRoutes');
 
 // create express app
 const app = express();
 
+// enable CORS
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+
 // parse the request body as JSON
 app.use(express.json());
 
-// enable CORS
-app.use(cors({ credentials: true, origin: 'http://localhost:5175' }));
 // parse the cookies
 app.use(cookieParser());
 
 app.use(logger);
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/posts', postRouter);
 
 // error route
 app.use(errorRoute);
