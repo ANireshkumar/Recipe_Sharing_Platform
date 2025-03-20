7ad7474a486382aad9489436d2a39537d4c366b1
ANireshkumar <niresh1606@gmail.com>
2025-03-20 15:57:53 +0530

    error fix1

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 
 // enable CORS
 app.use(cors({
-    origin: 'https://recipesharing1.netlify.app',
+    origin: 'http://localhost:5173','https://recipesharing1.netlify.app',
     credentials: true
 }));
 
